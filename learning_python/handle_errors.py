fb52013f796afd7e7866fac8b5ea6741df16d283
Gatete-Bruno <catobrunoisrael@gmail.com>
2021-06-22 12:30:09 +0200

    Py ACG

---
+++ learning_python/handle_errors.py
@@ -0,0 +1,18 @@
+import sys
+
+file_name = 'recipes.txt'
+
+try:
+    my_file = open(file_name, 'x+')
+    my_file.write(b'Meatballs\n')
+    my_file.close()
+except FileExistsError as err:
+    print(f"The {file_name} file already exists")
+    sys.exit(1)
+except:
+    print(f"Unable to write to the file")
+    sys.exit(1)
+else:
+    print(f"Wrote to {file_name}")
+finally:
+    print("Execution complete")
